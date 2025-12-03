bdaf68608de0580173c520744cb8e3906df9aa5d
salahhub <salahcvc45@gmail.com>
2025-12-04 00:17:37 +0100

    your commit message

--- src/main/java/com/blil/runnerz/run/RunController.java
+++ src/main/java/com/blil/runnerz/run/RunController.java
@@ -2,13 +2,13 @@ package com.blil.runnerz.run;
 
 import java.util.List;
 
-import org.checkerframework.checker.units.qual.A;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class RunController {
 
    private final RunRepository runRepository;
@@ -16,8 +16,14 @@ public class RunController {
     public RunController(RunRepository runRepository) {
         this.runRepository = runRepository;
     }
-    @GetMapping("/api/runs")
+
+    @GetMapping("")
     List<run> findAll(){
         return runRepository.findAll();
     }
+    @GetMapping("api/{id}")
+    run findById(@PathVariable Integer id){
+        return runRepository.findById(id);
+    }
+
 }
--- src/main/java/com/blil/runnerz/run/RunRepository.java
+++ src/main/java/com/blil/runnerz/run/RunRepository.java
@@ -3,9 +3,11 @@ package com.blil.runnerz.run;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.annotation.PostConstruct;
-import com.blil.runnerz.run.run;
+import org.springframework.stereotype.Repository;
 
+import jakarta.annotation.PostConstruct;
+
+@Repository
 public class RunRepository {
 
      private List<run> runs = new ArrayList<>();
@@ -14,7 +16,15 @@ public class RunRepository {
         return runs;
 
 } 
+
+run findById(int id) {
+    return runs.stream()
+            .filter(run -> run.id() == id)
+            .findFirst()
+            .get();
+}
 @PostConstruct
+
 public void initData() {
     runs.add(new run(1, "Morning Run", null, null, 5, Location.OUTDOOR));
     runs.add(new run(2, "Evening Run", null, null, 3, Location.INDOOR));
